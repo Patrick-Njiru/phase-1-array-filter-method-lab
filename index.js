ce1817cd0ab05e6e041163b3cd4b7611529a7d2f
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-08-21 13:00:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (drivers, driverName) => drivers.filter( name => name.toUpperCase() === driverName.toUpperCase())
+
+const fuzzyMatch = (drivers, driverName) => drivers.filter( name => name.slice(0,2) === driverName.slice(0,2))
+
+drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+]
+
+const matchName = (drivers, driverName) => drivers.filter (driverDetails => {
+    if (driverDetails.name === driverName) {
+        return driverDetails
+    }
+})
